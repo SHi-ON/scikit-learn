SHi-ON/scikit-learn

commit 64d6dbcd02c59731e9ee81a5bfdf453f050e4931

ENH: GridSearchCV can has predict_proba

sklearn/grid_search.py
@@ -324,6 +324,8 @@ class GridSearchCV(BaseEstimator):
         self.best_estimator = best_estimator
         if hasattr(best_estimator, 'predict'):
             self.predict = best_estimator.predict
+        if hasattr(best_estimator, 'predict_proba'):
+            self.predict = best_estimator.predict_proba
         if hasattr(best_estimator, 'score'):
             self.score = best_estimator.score
 

commit bb1b9ecb9c301b80fc5f9d8def944c119a249be1

FIX bug introduced in 64d6dbc

sklearn/grid_search.py
@@ -325,7 +325,7 @@ class GridSearchCV(BaseEstimator):
         if hasattr(best_estimator, 'predict'):
             self.predict = best_estimator.predict
         if hasattr(best_estimator, 'predict_proba'):
-            self.predict = best_estimator.predict_proba
+            self.predict_proba = best_estimator.predict_proba
         if hasattr(best_estimator, 'score'):
             self.score = best_estimator.score
 