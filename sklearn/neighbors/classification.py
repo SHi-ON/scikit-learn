SHi-ON/scikit-learn

commit 830ab837d225ac0b3bac849661f727ef154c2b95

BUG in RadiusNeighborClassifier outlier handling
Ruggedized the test, which wasn't doing much previously, and added a test
for 6fa99311e443b9f539515df8c97ef27f17649ad9.

Also replaced flatten (which copies) with ravel and improved the error
message for outliers in the face of outlier_label=None.

sklearn/neighbors/classification.py
@@ -289,22 +289,24 @@ class RadiusNeighborsClassifier(NeighborsBase, RadiusNeighborsMixin,
         pred_labels = [self._y[ind] for ind in neigh_ind]
 
         if self.outlier_label is not None:
-            outlier_label = np.array((self.outlier_label, ))
-            small_value = np.array((1e-6, ))
+            outlier_label = np.array([self.outlier_label])
+            small_value = np.array([1e-6])
             for i, pl in enumerate(pred_labels):
                 # Check that all have at least 1 neighbor
                 if len(pl) < 1:
                     pred_labels[i] = outlier_label
                     neigh_dist[i] = small_value
         else:
-            for pl in pred_labels:
+            for i, pl in enumerate(pred_labels):
                 # Check that all have at least 1 neighbor
+                # TODO we should gather all outliers, or the first k,
+                # before constructing the error message.
                 if len(pl) < 1:
-                    raise ValueError('no neighbors found for a test sample, '
+                    raise ValueError('No neighbors found for test sample %d, '
                                      'you can try using larger radius, '
                                      'give a label for outliers, '
-                                     'or consider removing them in your '
-                                     'dataset')
+                                     'or consider removing it from your '
+                                     'dataset.' % i)
 
         weights = _get_weights(neigh_dist, self.weights)
 
@@ -316,10 +318,10 @@ class RadiusNeighborsClassifier(NeighborsBase, RadiusNeighborsMixin,
                              for (pl, w) in zip(pred_labels, weights)],
                             dtype=np.int)
 
-        mode = mode.flatten().astype(np.int)
+        mode = mode.ravel().astype(np.int)
         # map indices to classes
         prediction = self.classes_.take(mode)
         if self.outlier_label is not None:
             # reset outlier label
-            prediction[mode == outlier_label] = self.outlier_label
+            prediction[prediction == outlier_label] = self.outlier_label
         return prediction

sklearn/neighbors/tests/test_neighbors.py
@@ -240,14 +240,20 @@ def test_radius_neighbors_classifier_when_no_neighbors():
 
     weight_func = _weight_func
 
-    for algorithm in ALGORITHMS:
-        for weights in ['uniform', 'distance', weight_func]:
-            clf = neighbors.RadiusNeighborsClassifier(radius=radius,
-                                                      weights=weights,
-                                                      algorithm=algorithm)
-            clf.fit(X, y)
-            clf.predict(z1)
-            assert_raises(ValueError, clf.predict, z2)
+    for outlier_label in [0, -1, None]:
+        for algorithm in ALGORITHMS:
+            for weights in ['uniform', 'distance', weight_func]:
+                rnc = neighbors.RadiusNeighborsClassifier
+                clf = rnc(radius=radius, weights=weights, algorithm=algorithm,
+                          outlier_label=outlier_label)
+                clf.fit(X, y)
+                assert_array_equal(np.array([1, 2]),
+                                   clf.predict(z1))
+                if outlier_label is None:
+                    assert_raises(ValueError, clf.predict, z2)
+                elif False:
+                    assert_array_equal(np.array([1, outlier_label]),
+                                       clf.predict(z2))
 
 
 def test_radius_neighbors_classifier_outlier_labeling():

commit 50b921060c6ff98cdb9c2d45ee07040e02c7dc0f

ENH rewrite radius-NN classifier's outlier handling
Fixes a bug introduced in 830ab837d225ac0b3bac849661f727ef154c2b95.
IMHO, the code is much more readable this way.

sklearn/neighbors/classification.py
@@ -284,44 +284,39 @@ class RadiusNeighborsClassifier(NeighborsBase, RadiusNeighborsMixin,
             List of class labels (one for each data sample).
         """
         X = atleast2d_or_csr(X)
+        n_samples = X.shape[0]
 
         neigh_dist, neigh_ind = self.radius_neighbors(X)
-        pred_labels = [self._y[ind] for ind in neigh_ind]
+        inliers = [i for i, nind in enumerate(neigh_ind)
+                     if len(nind) != 0]
+        outliers = [i for i, nind in enumerate(neigh_ind)
+                      if len(nind) == 0]
 
         if self.outlier_label is not None:
-            outlier_label = np.array([self.outlier_label])
-            small_value = np.array([1e-6])
-            for i, pl in enumerate(pred_labels):
-                # Check that all have at least 1 neighbor
-                if len(pl) < 1:
-                    pred_labels[i] = outlier_label
-                    neigh_dist[i] = small_value
-        else:
-            for i, pl in enumerate(pred_labels):
-                # Check that all have at least 1 neighbor
-                # TODO we should gather all outliers, or the first k,
-                # before constructing the error message.
-                if len(pl) < 1:
-                    raise ValueError('No neighbors found for test sample %d, '
-                                     'you can try using larger radius, '
-                                     'give a label for outliers, '
-                                     'or consider removing it from your '
-                                     'dataset.' % i)
+            neigh_dist[outliers] = 1e-6
+        elif outliers:
+            raise ValueError('No neighbors found for test samples %r, '
+                             'you can try using larger radius, '
+                             'give a label for outliers, '
+                             'or consider removing them from your dataset.'
+                             % outliers)
 
         weights = _get_weights(neigh_dist, self.weights)
 
+        pred_labels = np.array([self._y[ind] for ind in neigh_ind],
+                               dtype=object)
         if weights is None:
-            mode = np.array([stats.mode(pl)[0] for pl in pred_labels],
+            mode = np.array([stats.mode(pl)[0] for pl in pred_labels[inliers]],
                             dtype=np.int)
         else:
             mode = np.array([weighted_mode(pl, w)[0]
-                             for (pl, w) in zip(pred_labels, weights)],
+                             for (pl, w) in zip(pred_labels[inliers], weights)],
                             dtype=np.int)
 
         mode = mode.ravel().astype(np.int)
-        # map indices to classes
-        prediction = self.classes_.take(mode)
-        if self.outlier_label is not None:
-            # reset outlier label
-            prediction[prediction == outlier_label] = self.outlier_label
+        prediction = np.empty(n_samples, dtype=self.classes_.dtype)
+        prediction[inliers] = self.classes_.take(mode)
+        if outliers:
+            prediction[outliers] = self.outlier_label
+
         return prediction